3265186d73a833deeb00a25e26e073731df325d6
alfian666 <alfianarifianto666@gmail.com>
2021-04-04 09:48:00 +0700

    perbaiki coding

--- resources/views/genre.blade.php
+++ resources/views/genre.blade.php
@@ -6,26 +6,26 @@
 </head>
 <body style="background-color: azure;">
     <div class="container">
-		<div class="row">
-			<div class="col-md-4">
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h3 class="panel-title">Genre</h3>
-						<title>Genre</title>
-	<link rel="stylesheet" href="admin/assets/css/bootstrap.css">
-    <link rel="stylesheet" href="/css/form.css">
-    <link rel="stylesheet" href="/css/login.css">
-    <link rel="stylesheet" href="/css/style.css">
+	<h3 class="panel-title">Genre</h3>
+	<title>Genre</title>
 	<div class="panel-body">
 		<form method="post" class="form-horizontal">
 			<div class="form-group">
 				<label class="control-label col-md-3">ID</label>
-			<div class="col-md-7">
-				<input type="text" class="form-control" name="id" required>
-			</div>
-			<form method="post" class="form-horizontal">
-				<div class="form-group">
-					<label class="control-label col-md-3">Nama Genre</label>
 				<div class="col-md-7">
-					<input type="text" class="form-control" name="nama genre" required>
+					<input type="text" class="form-control" name="id" required>
 				</div>
+				<form method="post" class="form-horizontal">
+					<div class="form-group">
+						<label class="control-label col-md-3">Nama Genre</label>
+						<div class="col-md-7">
+							<input type="text" class="form-control" name="nama genre" required>
+						</div>
+					</div>
+				</form>
+			</div>
+		</form>
+	</div>
+</div>
+</body>
+</html>
